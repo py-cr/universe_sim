6dd458bbdbcc8022660cd61d621a5f901e652858
march3 <gang.wei@qq.com>
2023-04-05 09:38:19 +0800

    Python超人-宇宙模拟器

--- sim_scenes/funny/the_eye_of_god.py
+++ sim_scenes/funny/the_eye_of_god.py
@@ -1,35 +1,34 @@
 # -*- coding:utf-8 -*-
-# title           :组成文本的星球群（自定义星球）
-# description     :组成文本的星球群（自定义星球）
+# title           :组成上帝之眼的星球群（自定义星球）
+# description     :组成上帝之眼的星球群（自定义星球）
 # author          :Python超人
 # date            :2023-03-26
 # link            :https://gitcode.net/pythoncr/
 # python_version  :3.8
 # ==============================================================================
-from bodies import FixedStar, ColorBody
+from bodies import FixedStar, Body
 from common.consts import SECONDS_PER_HOUR, SECONDS_PER_DAY, SECONDS_PER_WEEK
 from sim_scenes.func import ursina_run
-from sim_scenes.interest.utils.body_utils import gen_bodies_from_image
+from sim_scenes.funny.utils.body_utils import gen_bodies_from_image
 
 
-def show_text_bodies():
+def show_eye_of_god():
     """
-    显示图片文本的星球群
+    上帝之眼
     """
     D = 6000  # 基本距离单位:km（随意赋值）
     # 观看摄像机的位置
     # camera_pos = (左-右+, 上+下-, 前+后-)
     camera_pos = (D, D, -7000 * D)
     # 根据 pixel_image 指定图片生成有色星球（注意：图片的像素不要太多）
-    bodies = gen_bodies_from_image(pixel_image="./images/python.png",
-                                   texture="color_body.png",
-                                   params={"camera_pos": camera_pos})
+    bodies = gen_bodies_from_image(pixel_image="./images/eye.png",
+                                         texture="color_body.jpg",
+                                         params={"camera_pos": camera_pos})
     # 放一个恒星作为背景
     bg = FixedStar(name="背景恒星", texture="fixed_star.png",
                    mass=2e32, color=(0xff, 0xf8, 0xd4),
-                   init_position=[-450 * D, 100 * D, 6000 * D],  # [ 左-右+, 上+下-, 远+近- ]
+                   init_position=[-400 * D, 100 * D, 6000 * D],  # [ 左-右+, 上+下-, 远+近- ]
                    ignore_mass=True)
-
     bodies.append(bg)
 
     # 使用 ursina 查看的运行效果
@@ -39,4 +38,4 @@ def show_text_bodies():
 
 
 if __name__ == '__main__':
-    show_text_bodies()
+    show_eye_of_god()

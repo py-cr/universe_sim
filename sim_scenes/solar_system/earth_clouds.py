95299c478f3066648c9b9916117a6d64aa3f20e8
march3 <gang.wei@qq.com>
2023-04-09 11:58:15 +0800

    Python超人-宇宙模拟器

--- sim_scenes/solar_system/earth_clouds.py
+++ sim_scenes/solar_system/earth_clouds.py
@@ -6,7 +6,8 @@
 # link            :https://gitcode.net/pythoncr/
 # python_version  :3.8
 # ==============================================================================
-from bodies import Earth
+from bodies import Earth, Sun
+from common.consts import AU
 from common.consts import SECONDS_PER_HOUR, SECONDS_PER_DAY
 from sim_scenes.func import ursina_run
 
@@ -15,16 +16,22 @@ if __name__ == '__main__':
     带有云层地球模拟
     """
     earth = Earth(texture="earth_hd.jpg",
-                  init_position=[0, 0, 0], init_velocity=[1, 0, 0],
-                  size_scale=1)
-    clouds = Earth(name="地球云层", texture="transparent_clouds.png",  # 纹理图使用了透明云层的图片
-                   size_scale=1.01, show_name=False, parent=earth)  # size_scale 要稍微比
+                  init_position=[0, 0, 0], init_velocity=[0, 0, 0],
+                  size_scale=1).set_ignore_mass()
 
-    bodies = [earth, clouds]
+    # 创建带有云层的地球
+    earth_with_clouds = Earth(texture="earth_hd.jpg",
+                              init_position=[1.5 * earth.diameter, 0, 0], init_velocity=[0, 0, 0],
+                              size_scale=1)
+    # 创建云层（texture纹理图使用了透明云层的图片，云层的 size_scale 要稍微比地球大一点）
+    clouds = Earth(name="地球云层", texture="transparent_clouds.png",
+                   size_scale=1.001, parent=earth_with_clouds)
+
+    bodies = [earth, earth_with_clouds, clouds]
 
     # 使用 ursina 查看的运行效果
     # 常用快捷键： P：运行和暂停  O：重新开始  I：显示天体轨迹
     # position = 左-右+、上+下-、前+后-
-    ursina_run(bodies, SECONDS_PER_HOUR / 2, position=(0, 0, -20000),
+    ursina_run(bodies, SECONDS_PER_HOUR / 2, position=(1.5 * earth.raduis, 0, -30000),
                # cosmic_bg="textures/cosmic1.jpg",
                view_closely=0.001)

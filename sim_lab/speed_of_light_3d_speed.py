1ea09c7e487dcdb2a55b595f3acd97d34898953e
march3 <gang.wei@qq.com>
2023-07-07 19:15:33 +0800

    Python超人-宇宙模拟器

--- sim_lab/speed_of_light_3d_speed.py
+++ sim_lab/speed_of_light_3d_speed.py
@@ -262,7 +262,7 @@ init.body_arrived = body_arrived
 # 使用 ursina 查看的运行效果
 # 常用快捷键： P：运行和暂停  O：重新开始  I：显示天体轨迹
 # position = 左-右+、上+下-、前+后-
-ursina_run(bodies, 100,
+ursina_run(bodies, 30,
            position=init.camera_position,
            # show_trail=init.show_trail,
            show_timer=True,

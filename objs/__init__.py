dbd7ab9c1a9c48458ee1ba79722603b9de48ebed
march3 <gang.wei@qq.com>
2023-07-29 22:42:32 +0800

    Python超人-宇宙模拟器

--- objs/__init__.py
+++ objs/__init__.py
@@ -6,3 +6,5 @@ from objs.space_ship import SpaceShip
 from objs.star_wars_speeder import StarWarsSpeeder
 from objs.rock_snow import RockSnow
 from objs.rock import Rock, create_rock
+from objs.core_valaga_clas import CoreValagaClas
+from objs.sci_fi_bomber import SciFiBomber
